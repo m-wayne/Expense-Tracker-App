0350206253919926a4918446071cbbe8c0ae95d7
m-wayne <mwayne@scribd.com>
2024-12-02 17:44:24 -0800

    Add UI test scheme

---
+++ Expense Tracker UI Tests/Expense_Tracker_UI_TestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Expense_Tracker_UI_TestsLaunchTests.swift
+//  Expense Tracker UI Tests
+//
+//  Created by Matthew Wayne on 12/2/24.
+//
+
+import XCTest
+
+final class Expense_Tracker_UI_TestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
